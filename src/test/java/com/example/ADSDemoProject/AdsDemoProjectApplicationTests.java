fa6bf6f63f6b490d824e6d85a16ea39ae127cb4e
Adam Waniak <adam.waniak@assecods.pl>
2019-07-05 11:14:36 +0200

    Set up h2db for testing

--- src/test/java/com/example/ADSDemoProject/AdsDemoProjectApplicationTests.java
+++ src/test/java/com/example/ADSDemoProject/AdsDemoProjectApplicationTests.java
@@ -1,16 +1,24 @@
 package com.example.ADSDemoProject;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AdsDemoProjectApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	DataSource dataSource;
 
+	@Test
+	public void shouldUsingH2DbForTesting() throws SQLException {
+		Assert.assertTrue(dataSource.getConnection().toString().contains("url=jdbc:h2:mem:db"));
+	}
 }
